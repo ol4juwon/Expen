835eaa53d804c3c403a7207fffb3ea90eaee1192
ol4juwon <olajuwonlawal2012@gmail.com>
2022-10-17 00:58:52 +0100

    Firebase 9 done

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,36 +1,46 @@
 import { useEffect, useRef, useState } from "react";
 import { db } from "../provider/config";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 export const useCollection = (c, _query, _orderBy) => {
 	const [error, setError] = useState(null);
 	const [documents, setDocuments] = useState(null);
 	const [loading, setLoading] = useState(false);
-	let query;
-	let orderBy;
+	let q;
+	let o;
 	if(_query)
-		query = useRef(_query);
+		q = useRef(_query).current;
 	if(_orderBy)
-		orderBy = useRef(_orderBy);
+		o = useRef(_orderBy).current;
 
 	useEffect(() => {
 		setLoading(true);
 		try{
-			const ref = collection(db, c);
-
-			const unsubscribe = onSnapshot(ref, (querySnapshot) => {
-				let results = [];
-				querySnapshot.docs.forEach((doc) => {
-					results.push({...doc.data(), id: doc.id});
+			let ref = collection(db, c);
+			console.log(typeof where, typeof query, );
+			if(q){
+				console.log("q", q);
+				ref = query(ref, where(...q));}
+			// if(o.current)
+			// 	ref = query(ref, orderBy(o.current.field, o.current.direction , limit(10)));
+			const unsubscribe = onSnapshot(ref, (snapshot) => {
+				let docs = [];
+				snapshot.forEach((doc) => {
+					docs.push({ id: doc.id, ...doc.data() });
 				});
-				setDocuments(results);
+				setDocuments(docs);
 				setLoading(false);
+				setError(null);
 			});
-			return () => unsubscribe();
+			return () => {
+				unsubscribe();
+			};
 		}catch(e){
+			console.log(e);
 			setError(e);
 			setLoading(false);
 		}
-	}, [c,query,orderBy]);
+		setLoading(false);
+	}, [c,q,o]);
 
 	return { documents, error, loading };
 };
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,25 @@
 import React from "react";
-// import { useAuthContext } from "../../hooks/useAuthContext";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import NewTransaction from "../../components/NewTransaction";
 import styles from  "./home.module.css";
-// import { useCollection } from "../../hooks/useCollection";
+import { useCollection } from "../../hooks/useCollection";
 import TransactionList from "../../components/TransactionList";
 import { LineWave } from "react-loader-spinner";
-import { useCollection } from "../../hooks/useCollection";
 const Home = () => {
-	// const {user} = useAuthContext();
-	const { documents, error } = useCollection("transactions", null, null);
-
-	// console.log(getDocument().then((res) => console.log(res)).catch());
-
+	const {user} = useAuthContext();
+	const { documents, error } = useCollection("transactions", ["uid", "==", user.uid], null);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
 				transaction List
-				{error && <div>{error}</div>}
+				{error && <div>{error.errorMessage}</div>}
 				{!documents && <LineWave />}
 				{ documents && <TransactionList trnx={documents} />
 				}
 			</div>
 			<div className={styles.sidebar}>
-				<NewTransaction uid={"ola"} />
+				<NewTransaction uid={user.uid} />
 			</div>
 		</div>
 	);
